7211f3e23a3af1a3ba4e1ac638f75082af5c4c4d
Sergey Diachkov <biometer2020@gmail.com>
2025-01-31 19:04:20 +0300

    Visual

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+function update_level() {
+    let level = document.getElementById('level-input').value
+    document.getElementById('level-status').innerText = level.toString()
+    console.log(level)
+}
